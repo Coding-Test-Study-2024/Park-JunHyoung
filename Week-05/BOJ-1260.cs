8df8be374b2925917ba46ae67204afa94c3ee764
Park JunHyoung <matte1428@naver.com>
2024-02-27 09:42:21 +0900

    feat : 백준 1260번(작성중)

---
+++ Week-05/BOJ-1260.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/* 백준 1260 : DFS와 BFS (https://www.acmicpc.net/problem/1260)
+ 
+ */
+namespace Week_05
+{
+    internal class BOJ_1260
+    {
+        static int N, M, V;
+        static bool[,] map;
+        static bool[] visited;
+
+        static void Main1()
+        {
+            string[] inputNMV = Console.ReadLine()!.Split();
+            N = int.Parse(inputNMV[0]);
+            M = int.Parse(inputNMV[1]);
+            V = int.Parse(inputNMV[2]);
+
+            map = new bool[N, N];
+            visited = new bool[N];
+
+            for(int i=0; i<M ; i++) {
+                string[] input = Console.ReadLine()!.Split();
+                int x = int.Parse(input[0]);
+                int y = int.Parse(input[1]);
+
+                map[x,y] = true;
+                map[y,x] = true;
+            }
+
+            InitVisited();
+
+        }
+
+        static void InitVisited()
+        {
+            visited = Enumerable.Repeat(false, N + 1).ToArray();
+        }
+
+        static void DFS()
+        {
+
+        }
+
+
+        static void BFS()
+        {
+
+        }
+    }
+}
