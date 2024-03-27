32cad3d90e5f787d705d36b41e72854bb1d5410e
Park JunHyoung <matte1428@naver.com>
2024-03-27 09:35:51 +0900

    feat : 백준 1026번

---
+++ Week-06/BOJ-1026.cs
@@ -0,0 +1,47 @@
+﻿
+//백준 1026 : 보물(https://www.acmicpc.net/problem/1026)
+namespace Week_06
+{
+    internal class BOJ_1026
+    {
+        static int N;
+        static int[] A, B;
+        static void Main1(string[] args)
+        {
+            GetInput();
+            Solve(A, B);
+        }
+        static void GetInput()
+        {
+            N = int.Parse(Console.ReadLine()!);
+
+            A = new int[N];
+            B = new int[N];
+
+            string[] input = Console.ReadLine()!.Split();
+            for (int i = 0; i < N; i++)
+            {
+                A[i] = int.Parse(input[i]);
+            }
+
+            input = Console.ReadLine()!.Split();
+            for (int i = 0; i < N; i++)
+            {
+                B[i] = int.Parse(input[i]);
+            }
+        }
+        static void Solve(int[] A, int[] B)
+        {
+            Array.Sort(A);
+            Array.Reverse(A);
+            Array.Sort(B);
+
+            int answer =0;
+            for(int i = 0; i < N;i++)
+            {
+                answer += A[i]*B[i];
+            }
+            Console.WriteLine(answer);
+        }
+    }
+}
