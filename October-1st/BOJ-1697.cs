d44094053924c730ace25580080c22d4905c2c78
Park JunHyoung <pj93251428@gmail.com>
2024-10-10 17:52:24 +0900

    feat : 백준 1697, 1764

---
+++ October-1st/BOJ-1697.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//백준 1697 - 숨바꼭질  https://www.acmicpc.net/problem/1697
+
+namespace October_1st
+{
+    internal class BOJ_1697
+    {
+        const int MAXIMUMCASE = 1000000;
+
+        public void Main()
+        {
+            string[] input = Console.ReadLine()!.Split();
+
+            int pointN = int.Parse(input[0]);
+            int pointK = int.Parse(input[1]);
+
+            BFS(pointN, pointK);
+        }
+
+
+        void BFS(int startPos, int targetPos)
+        {
+            //Init
+            int[] count = new int[MAXIMUMCASE + 1];
+            bool[] visited = new bool[MAXIMUMCASE + 1];
+            Queue<int> queue = new Queue<int>();
+            visited[startPos] = true;
+            queue.Enqueue(startPos);
+
+            if (startPos == targetPos)
+            {
+                Console.WriteLine(0);
+                return;
+            }
+
+
+            while (queue.Count > 0)
+            {
+                int curPos = queue.Dequeue();
+                int newPos;
+
+                //case 1 (move forward)
+                newPos = curPos + 1;
+                if (newPos >= 0 && newPos <= MAXIMUMCASE && !visited[newPos])
+                {
+                    queue.Enqueue(newPos);
+                    count[newPos] = count[curPos] + 1;
+                    visited[newPos] = true;
+
+                    if (newPos == targetPos)
+                    {
+                        Console.WriteLine(count[newPos]);
+                        return;
+                    }
+                }
+
+                //case 2 (move backward)
+                newPos = curPos - 1;
+                if (newPos >= 0 && newPos <= MAXIMUMCASE && !visited[newPos])
+                {
+                    queue.Enqueue(newPos);
+                    count[newPos] = count[curPos] + 1;
+                    visited[newPos] = true;
+
+                    if (newPos == targetPos)
+                    {
+                        Console.WriteLine(count[newPos]);
+                        return;
+                    }
+                }
+
+                //case 3 (teleportation)
+                newPos = curPos * 2;
+                if (newPos >= 0 && newPos <= MAXIMUMCASE && !visited[newPos])
+                {
+                    queue.Enqueue(newPos);
+                    visited[newPos] = true;
+                    count[newPos] = count[curPos] + 1;
+
+
+                    if (newPos == targetPos)
+                    {
+                        Console.WriteLine(count[newPos]);
+                        return;
+                    }
+                }
+            }
+        }
+
+    }
+}
---
+++ October-1st/BOJ-1764.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//백준 1764 - 듣보잡 https://www.acmicpc.net/problem/1764
+
+namespace October_1st
+{
+    internal class BOJ_1764
+    {
+        void Main()
+        {
+            string[] input = Console.ReadLine().Split();
+
+            int peopleNotHeardCount = int.Parse(input[0]);
+            int peopleNotSeenCount = int.Parse(input[1]);
+
+            string[] peopleNotHeard = new string[peopleNotHeardCount];
+            string[] peopleNotSeen = new string[peopleNotSeenCount];
+
+            for (int i = 0; i < peopleNotHeardCount; i++)
+            {
+                peopleNotHeard[i] = Console.ReadLine();
+            }
+
+            for (int i = 0; i < peopleNotSeenCount; i++)
+            {
+                peopleNotSeen[i] = Console.ReadLine();
+            }
+
+            HashSet<string> peopleNotHeardSet = new HashSet<string>(peopleNotHeard);
+            List<string> peopleNotHeardNotSeen = new List<string>();
+
+
+            foreach (string people in peopleNotSeen)
+            {
+                if (!peopleNotHeardSet.Add(people)) // 보지 못한 사람 셋에 추가가 안됨 == 중복임 == 듣도 보도 못한 사람임
+                {
+                    peopleNotHeardNotSeen.Add(people);
+                }
+            }
+
+            peopleNotHeardNotSeen.Sort();
+
+
+            Console.WriteLine(peopleNotHeardNotSeen.Count);
+
+            foreach (string output in peopleNotHeardNotSeen)
+            {
+                Console.WriteLine(output);
+            }
+        }
+
+    }
+}
