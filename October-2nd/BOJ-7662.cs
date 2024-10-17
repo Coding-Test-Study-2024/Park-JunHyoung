37a9843ad8cbd13dc5f674456c30401b9fa79ae2
Park JunHyoung <pj93251428@gmail.com>
2024-10-17 20:16:41 +0900

    fix : 백준 7662 시간초과
    1% -> 11% 에서 시간초과

--- October-2nd/BOJ-7662.cs
+++ October-2nd/BOJ-7662.cs
@@ -11,43 +11,45 @@ namespace October_2nd
 
             for(int i = 0; i < testDataCount; i++)
             {
-                GetTestData();
+                ProcessTestData();
             }
         }
 
-        static void GetTestData()
+        static void ProcessTestData()
         {
             int operationCount = int.Parse(Console.ReadLine()!); //k (k<=1,000,000)
 
-            SortedDictionary<int, int> sortedDictionary = new SortedDictionary<int, int>();
-            // Key : N, Value : count of N
+            // Key : n, Value : count of n
+            SortedList<int, int> sortedDictionary = new SortedList<int, int>();
 
-            for(int i=0; i < operationCount; i++)
+            // SortedList -> 11% 이후 시간초과
+            // SortedDictionary -> 1% 이후 시간초과
+
+            for (int i=0; i < operationCount; i++)
             {
                 string[] input = Console.ReadLine()!.Split();
-                string operation = input[0]; // I -> Insert n , D -> Delete max value or min value(next int n is 1 or-1)
+                string operation = input[0]; // I -> Insert n , D -> Delete min/max value(next int n is -1 or 1)
                 int n = int.Parse(input[1]);
 
-                if(operation == "I")
+                if(operation == "I") // work Insert
                 {
                     if (!sortedDictionary.ContainsKey(n)){ 
                         sortedDictionary.Add(n, 0);
                     }
                     sortedDictionary[n]++;
                 }
-                else // if (operation =="D")
+                else // if (operation =="D") // work Delete 
                 {
-                    int key;
-
                     if (sortedDictionary.Count == 0) continue;
+                    int key;
 
                     if(n == 1)
                     {
-                        key =sortedDictionary.Keys.Max();
+                        key =sortedDictionary.Keys.Last();
                     }
                     else // if (n==-1)
                     {
-                        key =sortedDictionary.Keys.Min();
+                        key =sortedDictionary.Keys.First();
                     }
 
                     //sortedDictionary.TryGetValue(key, out int temp);
@@ -63,7 +65,7 @@ namespace October_2nd
         }
 
         const string EMPTY = "EMPTY";
-        static void PrintData(SortedDictionary<int, int> sortedDictionary)
+        static void PrintData(SortedList<int, int> sortedDictionary)
         {
             if(sortedDictionary.Count ==0)
             {
@@ -74,6 +76,5 @@ namespace October_2nd
                 Console.WriteLine($"{sortedDictionary.Keys.Max()} {sortedDictionary.Keys.Min()}");
             }
         }
-
     }
 }
