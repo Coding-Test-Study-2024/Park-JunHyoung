5da191df816a91254c0993e60955912f8cfe43e4
Park JunHyoung <pj93251428@gmail.com>
2024-11-14 17:13:24 +0900

    feat : 백준 1717

---
+++ November-2nd/BOJ-1717.cs
@@ -0,0 +1,62 @@
+﻿namespace November_2nd
+{
+    internal class BOJ_1717
+    {
+        const string NO = "NO";
+        const string YES = "YES";
+        static int[] parent;
+
+
+        static void Main()
+        {
+
+            string[] input = Console.ReadLine()!.Split(); // N,M
+            int N = int.Parse(input[0]); // N
+            int testCaseCount = int.Parse(input[1]); // M
+
+            parent = new int[N+1];
+            for(int i = 0; i <=N; i++)
+                parent[i] = i;
+
+            for(int  i = 0; i < testCaseCount; i++)
+            {
+                input = Console.ReadLine()!.Split();
+
+                int a = int.Parse(input[1]);
+                int b = int.Parse(input[2]);
+
+                if (input[0] == "0")
+                {
+                    Union(a, b);
+                }else //if (input[0] == "1")
+                {
+                    Console.WriteLine(IsContain(a, b) ? YES : NO);
+                }
+            }
+        }
+
+        static void Union(int a, int b)
+        {
+            int rootA = Find(a);
+            int rootB = Find(b);
+
+            if(rootA != rootB)
+                parent[rootB] = rootA;
+        }
+
+        static bool IsContain(int a, int b)
+        {
+            return Find(a) == Find(b);
+        }
+
+        // Find Parent
+        static int Find(int x)
+        {
+            if (parent[x] != x)
+            {
+                parent[x] = Find(parent[x]);
+            }
+            return parent[x];
+        }
+    }
+}
